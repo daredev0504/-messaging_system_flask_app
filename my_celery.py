4776f87f8d0e7aa6f1aefb2132cb920579ed926b
Peter Adebimpe <Stephiepoussin2100@gmail.com>
2024-07-13 11:19:27 +0100

    fil4245r5fffsfgr8

--- my_celery.py
+++ my_celery.py
@@ -1,15 +1,7 @@
 from celery import Celery
-
+import app
 # Broker URL format: 'amqp://username:password@host:port/virtual_host'
 broker_url = 'amqp://tosingh:peranofo54@172.161.146.165:5672/rabbit'
 
 # Create Celery instance
-celery_app = Celery('tasks', broker=broker_url)
-
-# Optional configuration
-celery_app.conf.update(
-    result_backend='rpc://',
-    task_serializer='json',
-    accept_content=['json'],  # Ignore other content types
-    timezone='UTC',
-)
+celery_app = Celery(app.name, broker=broker_url)
