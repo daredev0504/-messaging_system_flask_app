37453eaa0d4aa769d5f92b5c9d0ce9aa591ceb69
Peter Adebimpe <Stephiepoussin2100@gmail.com>
2024-07-13 10:35:07 +0100

    fil4245r5f8

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from my_celery import celery_app
 from tasks import send_email_task
 import logging
+import urllib.parse
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='/var/log/messaging_system.log', level=logging.INFO)
@@ -13,18 +14,25 @@ app = FastAPI()
 @app.get('/sendmail')
 def sendmail(recipient: str):
     try:
+        # Decode the recipient URL-encoded string
+        recipient_email = urllib.parse.unquote(recipient)
         recipient_email = recipient
 
-        # Example URL: /sendmail?recipient=mailto:destiny@destinedcodes.com
-        if recipient.startswith('mailto:'):
-            recipient_email = recipient.replace('mailto:', '')
-
-            # Queue email task in Celery
-            send_email_task.delay(recipient_email, "I sent you an email regarding my rabbit mq and celery project")
-
-            return {'message': 'Email sending task queued.'}
+         # Example URL: /sendmail?recipient=tosinghdarey2100%40gmail.com
+        if recipient_email.startswith('mailto:'):
+            recipient_email = recipient_email.replace('mailto:', '')
         else:
+            recipient_email = recipient_email
+
+        # Validate if recipient_email is a valid email format (optional)
+        import re
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", recipient_email):
             return {'error': 'Invalid recipient format.'}
+
+         # Queue email task in Celery
+        send_email_task.delay(recipient_email, "I sent you an email regarding my rabbit mq and celery project")
+
+        return {'message': 'Email sending task queued.'}
 
     except Exception as e:
         logger.error(f"Failed to queue email task: {str(e)}")
