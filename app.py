4b63e9ccf612be1e49f4e1301eb64e4b08d22424
Peter Adebimpe <Stephiepoussin2100@gmail.com>
2024-07-13 11:10:15 +0100

    fil4245r5fffsfr8

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 
 from fastapi import FastAPI
 from my_celery import celery_app
-from tasks import send_email_task
+#from tasks import send_email_task
+import smtplib
+from email.message import EmailMessage
 import logging
 import urllib.parse
 
@@ -10,6 +12,34 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename='/var/log/messaging_system.log', level=logging.INFO)
 
 app = FastAPI()
+
+@celery_app.task
+def send_email_task(recipient_email, message):
+    try:
+        # SMTP server configuration (adjust as per your SMTP server)
+        smtp_server = 'smtp.gmail.com'
+        smtp_port = 587
+        smtp_user = 'petoski.ade@gmail.com'
+        smtp_password = 'kdqrxllljmvvxcvh'
+
+        # Compose email message
+        msg = EmailMessage()
+        msg.set_content(message)
+        msg['Subject'] = 'Messaging using RabbitMQ and Celery'
+        msg['From'] = smtp_user
+        msg['To'] = recipient_email
+
+        # Send email
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(smtp_user, smtp_password)
+            server.send_message(msg)
+        
+        logger.info(f"Email sent to {recipient_email}")
+
+    except Exception as e:
+        logger.error(f"Failed to send email to {recipient_email}: {str(e)}")
+
 
 @app.get('/sendmail')
 def sendmail(recipient: str):
