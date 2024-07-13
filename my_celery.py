949a0f37ffc5530380135dcbbe2ef41390e09946
Peter Adebimpe <Stephiepoussin2100@gmail.com>
2024-07-13 13:05:18 +0100

    xcdfg

--- my_celery.py
+++ my_celery.py
@@ -4,8 +4,8 @@ from celery import Celery
 broker_url = 'amqp://tosingh:peranofo54@172.161.146.165:5672/rabbit'
 
 # Create Celery instance
-celery_app = Celery('messaging_system_flask_app', broker=broker_url)
+celery_app = Celery('app', broker=broker_url)
 
 
 # Autodiscover tasks
-celery_app.autodiscover_tasks(['messaging_system_flask_app'], related_name='tasks', force=True)
+celery_app.autodiscover_tasks(['app'], related_name='tasks', force=True)
