5b882aa8ec6687a09f30d014a47e727a68a1ae70
Peter Adebimpe <Stephiepoussin2100@gmail.com>
2024-07-13 10:13:26 +0100

    fil424558

--- my_celery.py
+++ my_celery.py
@@ -1,7 +1,7 @@
 from celery import Celery
 
 # Broker URL format: 'amqp://username:password@host:port/virtual_host'
-broker_url = 'amqp://guest:guest@172.161.146.165:5672//'
+broker_url = 'amqp://tosingh:peranofo54@172.161.146.165:5672/rabbit'
 
 # Create Celery instance
 celery_app = Celery('tasks', broker=broker_url)
