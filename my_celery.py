8f07f445504a659dc0fde338d32cfc357105baf0
Peter Adebimpe <Stephiepoussin2100@gmail.com>
2024-07-13 11:53:15 +0100

    fil4245r5fffsfgrrdfgfhdsff8

--- my_celery.py
+++ my_celery.py
@@ -4,8 +4,8 @@ from celery import Celery
 broker_url = 'amqp://tosingh:peranofo54@172.161.146.165:5672/rabbit'
 
 # Create Celery instance
-celery_app = Celery('app', broker=broker_url)
+celery_app = Celery('messaging_system_flask_app', broker=broker_url)
 
 
 # Register tasks
-celery_app.autodiscover_tasks(['messaging_system_flask_app'])  # Replace 'app' with your module name
+celery_app.autodiscover_tasks(['tasks'])  # Replace 'app' with your module name
