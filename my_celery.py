c5efa4cb16ed1c5cdb4e0377093c5ed393033829
Peter Adebimpe <Stephiepoussin2100@gmail.com>
2024-07-13 12:19:49 +0100

    fil4245r5fffsfgrrdfgfhdsdff8

--- my_celery.py
+++ my_celery.py
@@ -1,11 +1,12 @@
 from celery import Celery
+from tasks import send_email_task
 
 # Broker URL format: 'amqp://username:password@host:port/virtual_host'
 broker_url = 'amqp://tosingh:peranofo54@172.161.146.165:5672/rabbit'
 
 # Create Celery instance
-celery_app = Celery('messaging_system_flask_app', broker=broker_url)
+celery_app = Celery('app', broker=broker_url)
 
 
 # Register tasks
-celery_app.autodiscover_tasks(['tasks'])  # Replace 'app' with your module name
+celery_app.task.Register(send_email_task) 
